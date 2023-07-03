083854edb3e63f7cc436b7484c94040d99af3513
tyronmaster <degonski.art@gmail.com>
2023-07-03 22:37:51 +0300

    feat: implement table testing

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,48 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+
+  { a: 1, b: 2, action: Action.Subtract, expected: -1 },
+  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+
+  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
+  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
+  { a: 3, b: 0, action: Action.Multiply, expected: 0 },
+
+  { a: 2, b: 1, action: Action.Divide, expected: 2 },
+  { a: 2, b: 2, action: Action.Divide, expected: 1 },
+  { a: 3, b: 2, action: Action.Divide, expected: 1.5 },
+  { a: 3, b: 0, action: Action.Divide, expected: Infinity },
+
+  { a: 2, b: 1, action: Action.Exponentiate, expected: 2 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 3, b: 0, action: Action.Exponentiate, expected: 1 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'Should calculate correctly simpleCalculator(%d, %d, %s)',
+    ({
+      a,
+      b,
+      action,
+      expected,
+    }: {
+      a: number;
+      b: number;
+      action: string;
+      expected: number;
+    }) => {
+      if (action === Action.Divide) {
+        expect(simpleCalculator({ a, b, action })).toBeCloseTo(expected);
+      } else {
+        expect(simpleCalculator({ a, b, action })).toBe(expected);
+      }
+    },
+  );
 });
